0bb58a7548de03f6c7fe8ae2e63021f67d72713e
Douglas <douglaswcastro@hotmail.com>
2023-01-22 09:29:01 -0300

    aula 2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,17 @@
-import { Habit } from './components/Habit'
+import './styles/global.css'
+//import { Habit } from './components/Habit'
+import logoImage from './assets/logo.svg'
 
-function App() {
+export function App() {
   return (
-    <div>
-      <Habit completed={3}/>
-      <Habit completed={10}/>
-      <Habit completed={20}/>
-      <Habit completed={30}/>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex-col gap-16"> 
+        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+          <img src={logoImage} alt="Habits"/>
+          <button type="button"> Novo Hábito</button>
+        </div>
+      </div>
     </div>
     
   )
 }
-
-export default App
-
-// Componente: Reaproveitar / isolar
-// Propriedade: Uma informação enviada para modificar um componente visual ou comportamental
