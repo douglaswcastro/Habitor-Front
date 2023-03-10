15307e59432d8257a2ca9a722bdce796720bc22c
Douglas <douglaswcastro@hotmail.com>
2023-03-09 23:12:33 -0300

    adicionando mais coisas no front

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,17 @@
 import './styles/global.css'
 //import { Habit } from './components/Habit'
 import logoImage from './assets/logo.svg'
+import {Plus} from 'phosphor-react'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-full max-w-5xl px-6 flex-col gap-16"> 
-        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
-          <img src={logoImage} alt="Habits"/>
-          <button type="button"> Novo Hábito</button>
-        </div>
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16"> 
+
+        <Header />
+        <SummaryTable/>
       </div>
     </div>
     
---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,9 @@
+export function SummaryTable(){
+    return (
+        <div className="w-full flex">
+            <div className="grid grid-rows-7 grid-flow-row gap-3">
+                <div className="text-zinc-400"></div>
+            </div>
+        </div>
+    )
+}
